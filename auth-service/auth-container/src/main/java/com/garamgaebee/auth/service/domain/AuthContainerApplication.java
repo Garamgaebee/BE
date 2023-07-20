df65feb53f4e885dad99bade4351dd60e1e29cc6
seock1000 <tjrcjs0705@gmail.com>
2023-07-20 10:54:59 +0900

    [FEAT] auth service 멀티모듈 초기 구성

---
+++ auth-service/auth-container/src/main/java/com/garamgaebee/auth/service/domain/AuthContainerApplication.java
@@ -0,0 +1,13 @@
+package com.garamgaebee.auth.service.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthContainerApplication.class, args);
+    }
+
+}
---
+++ auth-service/auth-messaging/src/test/java/com/garamgaebee/auth/service/AuthMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garamgaebee.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
