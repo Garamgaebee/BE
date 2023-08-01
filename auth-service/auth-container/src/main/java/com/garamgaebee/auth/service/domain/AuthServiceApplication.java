0e934d793ef928207a961b24d35445c6d57250ae
seock1000 <tjrcjs0705@gmail.com>
2023-08-01 11:03:09 +0900

    [FEAT] : Oauth2 server 접근 web client adapter 구현

--- auth-service/auth-container/src/main/java/com/garamgaebee/auth/service/domain/AuthServiceApplication.java
+++ auth-service/auth-container/src/main/java/com/garamgaebee/auth/service/domain/AuthServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = { "com.garamgaebee"})
-@SpringBootApplication(scanBasePackages = { "com.garamgaebee"})
+@SpringBootApplication(scanBasePackages = "com.garamgaebee")
 @EntityScan(basePackages = { "com.garamgaebee"})
 @EnableJpaAuditing
 public class AuthServiceApplication {
--- auth-service/auth-messaging/auth-webclient-messaging/src/main/java/com/garamgaebee/auth/service/messaging/adapter/Oauth2RequestClientImpl.java
+++ auth-service/auth-messaging/auth-webclient-messaging/src/main/java/com/garamgaebee/auth/service/messaging/adapter/Oauth2RequestClientImpl.java
@@ -3,19 +3,24 @@ package com.garamgaebee.auth.service.messaging.adapter;
 import com.garamgaebee.auth.service.domain.config.OauthProvider;
 import com.garamgaebee.auth.service.domain.dto.oauth.OauthToken;
 import com.garamgaebee.auth.service.domain.port.output.web.Oauth2RequestClient;
+import com.garamgaebee.auth.service.messaging.publisher.Oauth2RequestWebClientPublisher;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class Oauth2RequestClientImpl implements Oauth2RequestClient {
+
+    private final Oauth2RequestWebClientPublisher publisher;
     @Override
     public OauthToken getToken(String code, OauthProvider provider) {
-        return null;
+        return publisher.requestAccessTokenToAuthorizationServer(code, provider);
     }
 
     @Override
     public Map<String, Object> getUserAttributes(OauthProvider provider, OauthToken token) {
-        return null;
+        return publisher.requestUserProfileToResourceServer(provider, token);
     }
 }
