8235f9976f13cef33e6598de229f909bc3d925fd
seock1000 <tjrcjs0705@gmail.com>
2023-08-16 16:14:41 +0900

    [FEAT] : Eureka Server 구축

--- auth-service/auth-container/src/main/java/com/garamgaebee/auth/service/domain/AuthServiceApplication.java
+++ auth-service/auth-container/src/main/java/com/garamgaebee/auth/service/domain/AuthServiceApplication.java
@@ -3,6 +3,7 @@ package com.garamgaebee.auth.service.domain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = "com.garamgaebee")
 @EntityScan(basePackages = { "com.garamgaebee"})
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
