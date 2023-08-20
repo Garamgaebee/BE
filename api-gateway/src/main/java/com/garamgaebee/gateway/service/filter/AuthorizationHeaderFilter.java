546622b61d4451d732ab99cb78892642787a6dbd
seock1000 <tjrcjs0705@gmail.com>
2023-08-20 15:44:32 +0900

    temp commit

---
+++ api-gateway/src/main/java/com/garamgaebee/gateway/service/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,67 @@
+package com.garamgaebee.gateway.service.filter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+        // application.yml 파일에서 지정한 filer의 Argument값을 받는 부분
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String token = exchange.getRequest().getHeaders().get("Authorization").get(0).substring(7);   // 헤더의 토큰 파싱 (Bearer 제거)
+
+            System.out.println(token);
+            //TODO openFeign으로 jwt token 검증
+
+            return chain.filter(exchange);
+        };
+    }
+
+    // 토큰 검증 요청을 실행하는 도중 예외가 발생했을 때 예외처리하는 핸들러
+    @Bean
+    public ErrorWebExceptionHandler tokenValidation() {
+        return new JwtTokenExceptionHandler();
+    }
+
+    // 실제 토큰이 null, 만료 등 예외 상황에 따른 예외처리
+    public class JwtTokenExceptionHandler implements ErrorWebExceptionHandler {
+        private String getErrorCode(int errorCode) {
+            return "";//"{\\"errorCode\\":" + errorCode + "}";
+        }
+
+        @Override
+        public Mono<Void> handle(
+                ServerWebExchange exchange, Throwable ex) {
+            int errorCode = 500;
+            if (ex.getClass() == NullPointerException.class) {
+                errorCode = 100;
+            } else if (ex.getClass() == ExpiredJwtException.class) {
+                errorCode = 200;
+            }
+
+            byte[] bytes = getErrorCode(errorCode).getBytes(StandardCharsets.UTF_8);
+            DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+            return exchange.getResponse().writeWith(Flux.just(buffer));
+        }
+    }
+}
