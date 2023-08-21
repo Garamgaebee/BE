f99c8ccedb15bf1335657f73486a2b6c422bcecd
peter <97747863+wcorn@users.noreply.github.com>
2023-08-21 15:34:58 +0900

    [ADD] 이미지 서버 유레카 추가

--- image-service/image-container/src/main/java/com/garamgaebee/imagecontainer/ImageContainerApplication.java
+++ image-service/image-container/src/main/java/com/garamgaebee/imagecontainer/ImageContainerApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.TimeZone;
 
 @SpringBootApplication(scanBasePackages = { "com.garamgaebee"})
 @EntityScan(basePackages = { "com.garamgaebee"})
+@EnableDiscoveryClient
 public class ImageContainerApplication {
     @PostConstruct
     public void startedTimeZoneSet() {
