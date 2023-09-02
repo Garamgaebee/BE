30e414ab37386407b5e0c75385a5c188591af46f
destiny3912 <destiny3910@gmail.com>
2023-09-02 11:15:34 +0900

    [ADD] localhost profile

--- ec2run.sh
+++ ec2run.sh
@@ -7,12 +7,14 @@ sudo docker pull rookie3912/imageservice
 sudo docker pull rookie3912/memberservice
 sudo docker pull rookie3912/teamservice
 sudo docker pull rookie3912/threadservice
+sudo docker pull redis
 
-sudo nohup docker run -p 9000:9000 rookie3912/eureka > eureka.out 2>&1 &
-sudo nohup docker run -p 8080:8080 rookie3912/gateway > gateway.out 2>&1 &
-sudo nohup docker run -p 8081:8081 rookie3912/authservice > authservice.out 2>&1 &
-sudo nohup docker run -p 9001:9001 rookie3912/imageservice > imageservice.out 2>&1 &
-sudo nohup docker run -p 8082:8082 rookie3912/memberservice > memberservice.out 2>&1 &
-sudo nohup docker run -p 8083:8083 rookie3912/teamservice > teamservice.out 2>&1 &
-sudo nohup docker run -p 8084:8084 rookie3912/threadservice > threadservice.out 2>&1 &
+sudo nohup docker run -p 9000:9000 rookie3912/eureka --add-host=host.docker.internal:host-gateway > eureka.out 2>&1 &
+sudo nohup docker run -p 8080:8080 rookie3912/gateway --add-host=host.docker.internal:host-gateway > gateway.out 2>&1 &
+sudo nohup docker run -p 8081:8081 rookie3912/authservice --add-host=host.docker.internal:host-gateway > authservice.out 2>&1 &
+sudo nohup docker run -p 9001:9001 rookie3912/imageservice --add-host=host.docker.internal:host-gateway > imageservice.out 2>&1 &
+sudo nohup docker run -p 8082:8082 rookie3912/memberservice --add-host=host.docker.internal:host-gateway > memberservice.out 2>&1 &
+sudo nohup docker run -p 8083:8083 rookie3912/teamservice --add-host=host.docker.internal:host-gateway > teamservice.out 2>&1 &
+sudo nohup docker run -p 8084:8084 rookie3912/threadservice --add-host=host.docker.internal:host-gateway > threadservice.out 2>&1 &
+sudo nohup docker run -p 6379:6379 redis > redis.out 2>&1 &
 
