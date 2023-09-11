d10bd41b8fd97cd28f6a00971b52ea5bdbe7fd87
seock1000 <tjrcjs0705@gmail.com>
2023-09-12 04:36:03 +0900

    [FIX] : swagger test에서 cors 문제 해결 및 member-service, thread-service DB 연결 관련 설정 에러 해결

--- member-service/member-container/src/main/java/com/garamgaebee/member/container/MemberServiceApplication.java
+++ member-service/member-container/src/main/java/com/garamgaebee/member/container/MemberServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
