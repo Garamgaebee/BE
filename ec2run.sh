4f798e3ac845c828d61583e4a9314b055fb98ae6
염석천 <90550303+seock1000@users.noreply.github.com>
2023-09-21 18:18:00 +0900

    [FIX] : deploy 관련 error 수정 (#47)
    * [FIX] : deploy 관련 error 수정
    
    * [FIX] : swagger test에서 cors 문제 해결 및 member-service, thread-service DB 연결 관련 설정 에러 해결

--- ec2run.sh
+++ ec2run.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #ec2 명령어 백업용 (실제 사용 x)
-sudo docker pull rookie3912/eureka
-sudo docker pull rookie3912/gateway
-sudo docker pull rookie3912/authservice
-sudo docker pull rookie3912/imageservice
-sudo docker pull rookie3912/memberservice
-sudo docker pull rookie3912/teamservice
-sudo docker pull rookie3912/threadservice
+sudo docker pull tjrcjs0705/eureka
+sudo docker pull tjrcjs0705/gateway
+sudo docker pull tjrcjs0705/authservice
+sudo docker pull tjrcjs0705/imageservice
+sudo docker pull tjrcjs0705/memberservice
+sudo docker pull tjrcjs0705/teamservice
+sudo docker pull tjrcjs0705/threadservice
 sudo docker pull redis
 
-sudo nohup docker run -p 9000:9000 rookie3912/eureka --add-host=host.docker.internal:host-gateway > eureka.out 2>&1 &
-sudo nohup docker run -p 8080:8080 rookie3912/gateway --add-host=host.docker.internal:host-gateway > gateway.out 2>&1 &
-sudo nohup docker run -p 8081:8081 rookie3912/authservice --add-host=host.docker.internal:host-gateway > authservice.out 2>&1 &
-sudo nohup docker run -p 9001:9001 rookie3912/imageservice --add-host=host.docker.internal:host-gateway > imageservice.out 2>&1 &
-sudo nohup docker run -p 8082:8082 rookie3912/memberservice --add-host=host.docker.internal:host-gateway > memberservice.out 2>&1 &
-sudo nohup docker run -p 8083:8083 rookie3912/teamservice --add-host=host.docker.internal:host-gateway > teamservice.out 2>&1 &
-sudo nohup docker run -p 8084:8084 rookie3912/threadservice --add-host=host.docker.internal:host-gateway > threadservice.out 2>&1 &
+sudo nohup docker run --add-host=host.docker.internal:host-gateway -p 9000:9000 tjrcjs0705/eureka > eureka.out 2>&1 &
+sudo nohup docker run --add-host=host.docker.internal:host-gateway -p 8080:8080 tjrcjs0705/gateway > gateway.out 2>&1 &
+sudo nohup docker run --add-host=host.docker.internal:host-gateway -p 8081:8081 tjrcjs0705/authservice > authservice.out 2>&1 &
+sudo nohup docker run --add-host=host.docker.internal:host-gateway -p 9001:9001 tjrcjs0705/imageservice > imageservice.out 2>&1 &
+sudo nohup docker run --add-host=host.docker.internal:host-gateway -p 8082:8082 tjrcjs0705/memberservice > memberservice.out 2>&1 &
+sudo nohup docker run --add-host=host.docker.internal:host-gateway -p 8083:8083 tjrcjs0705/teamservice > teamservice.out 2>&1 &
+sudo nohup docker run --add-host=host.docker.internal:host-gateway -p 8084:8084 tjrcjs0705/threadservice > threadservice.out 2>&1 &
 sudo nohup docker run -p 6379:6379 redis > redis.out 2>&1 &
 
