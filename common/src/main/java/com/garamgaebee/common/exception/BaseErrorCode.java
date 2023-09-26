b10fcbc643890c16887547c1e58d4e155c5787ef
염석천 <90550303+seock1000@users.noreply.github.com>
2023-09-27 00:26:14 +0900

    59 auth service api documentation (#60)
    * [DOCS]: auth-service api 문서 작성
    
    * [DOCS] : swagger에 jwt header 추가

--- common/src/main/java/com/garamgaebee/common/exception/BaseErrorCode.java
+++ common/src/main/java/com/garamgaebee/common/exception/BaseErrorCode.java
@@ -26,7 +26,7 @@ public enum BaseErrorCode {
     NOT_REGISTERED_MEMBER(false, HttpStatus.BAD_REQUEST, "등록되지 않은 유저입니다."),
     ALREADY_EXIST_EMAIL(false, HttpStatus.BAD_REQUEST, "이미 가입된 이메일입니다."),
     ALREADY_EXIST_NICKNAME(false, HttpStatus.BAD_REQUEST, "이미 사용 중인 닉네임입니다."),
-    WRONG_AUTHORIZATION_CODE(false, HttpStatus.BAD_REQUEST, "인증 코드가 일치하지 않습니다."),
+    WRONG_AUTHORIZATION_EMAIL(false, HttpStatus.BAD_REQUEST, "인증코드가 존재하지 않는 이메일입니다."),
     WRONG_ID_PASSWORD(false, HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 일치하지 않습니다."),
     INVALID_REFRESH_TOKEN(false, HttpStatus.BAD_REQUEST, "유효하지 않은 Refresh Token입니다."),
 
