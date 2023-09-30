12668689376869d3ca967c8fce9e675227a6d988
peter <dsk08208@gmail.com>
2023-09-30 14:24:00 +0900

    [CHORE] team-service  reseponse 수정 (#67)

--- team-service/team-application/src/main/java/com/garamgaebee/teamapplication/rest/TeamController.java
+++ team-service/team-application/src/main/java/com/garamgaebee/teamapplication/rest/TeamController.java
@@ -1,5 +1,6 @@
 package com.garamgaebee.teamapplication.rest;
 
+import com.garamgaebee.common.response.BaseResponse;
 import com.garamgaebee.teamapplicationservice.dto.command.*;
 import com.garamgaebee.teamapplicationservice.dto.request.EditTeamRequest;
 import com.garamgaebee.teamapplicationservice.dto.response.*;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,12 +33,11 @@ public class TeamController {
      *
      * @param teamId   팀 id
      * @param memberId 멤버 id
-     * @return
      */
     @GetMapping(value = "/{teamId}")
-    public ResponseEntity<GetMainPageResponse> getMainPage(@PathVariable UUID teamId, @RequestParam UUID memberId) {
+    public ResponseEntity<BaseResponse<GetMainPageResponse>> getMainPage(@PathVariable UUID teamId, @RequestParam UUID memberId) {
         GetMainPageResponse getMainPageResponse = teamApplicationService.getMainPage(new GetMainPageCommand(teamId, memberId));
-        return ResponseEntity.ok(getMainPageResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(getMainPageResponse));
     }
 
     /**
@@ -44,7 +45,6 @@ public class TeamController {
      *
      * @param teamId   팀 id
      * @param memberId 멤버 id
-     * @return
      */
     @Operation(summary = "팀 공지사항 작성")
     @ApiResponses(value = {
@@ -56,12 +56,12 @@ public class TeamController {
                             "최대 이미지 수를 초과하였습니다.")
     })
     @PostMapping(value = "/{teamId}/notification", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<CreateNotificationResponse> createNotification(@Parameter(name = "teamId", description = "team's' id", in = ParameterIn.PATH) @PathVariable UUID teamId,
+    public ResponseEntity<BaseResponse<CreateNotificationResponse>> createNotification(@Parameter(name = "teamId", description = "team's' id", in = ParameterIn.PATH) @PathVariable UUID teamId,
                                                                          @Parameter(name = "memberId", description = "member's' id", in = ParameterIn.QUERY) @RequestParam UUID memberId,
                                                                          @Parameter(name = "image", description = "image file", in = ParameterIn.DEFAULT) @RequestPart(value = "image", required = false) List<MultipartFile> images,
                                                                          @Parameter(name = "content", description = "notification's content", in = ParameterIn.DEFAULT) @RequestPart String content) {
         CreateNotificationResponse createNotificationResponse = teamApplicationService.createNotification(new CreateNotificationCommand(teamId, memberId, images, content));
-        return ResponseEntity.ok(createNotificationResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(createNotificationResponse));
     }
 
     /**
@@ -69,7 +69,6 @@ public class TeamController {
      *
      * @param teamId   팀 id
      * @param memberId 멤버 id
-     * @return
      */
     @Operation(summary = "팀 종료하기")
     @ApiResponses(value = {
@@ -78,10 +77,10 @@ public class TeamController {
                     "팀을 찾을 수 없습니다.")
     })
     @DeleteMapping(value = "/{teamId}")
-    public ResponseEntity<DoneTeamResponse> doneTeam(@Parameter(name = "teamId", description = "team's' id", in = ParameterIn.PATH) @PathVariable UUID teamId,
+    public ResponseEntity<BaseResponse<DoneTeamResponse>> doneTeam(@Parameter(name = "teamId", description = "team's' id", in = ParameterIn.PATH) @PathVariable UUID teamId,
                                                      @Parameter(name = "memberId", description = "member's' id", in = ParameterIn.QUERY) @RequestParam UUID memberId) {
         DoneTeamResponse doneTeamResponse = teamApplicationService.doneTeam(new DoneTeamCommand(teamId, memberId));
-        return ResponseEntity.ok(doneTeamResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(doneTeamResponse));
     }
 
     /**
@@ -89,7 +88,6 @@ public class TeamController {
      *
      * @param teamId   팀 id
      * @param memberId 멤버 id
-     * @return
      */
     @Operation(summary = "팀 종료하기")
     @ApiResponses(value = {
@@ -98,10 +96,10 @@ public class TeamController {
                     "팀을 찾을 수 없습니다.")
     })
     @DeleteMapping(value = "/{teamId}/members/{memberId}")
-    public ResponseEntity<ExitTeamResponse> exitTeam(@Parameter(name = "teamId", description = "team's' id", in = ParameterIn.PATH) @PathVariable UUID teamId,
+    public ResponseEntity<BaseResponse<ExitTeamResponse>> exitTeam(@Parameter(name = "teamId", description = "team's' id", in = ParameterIn.PATH) @PathVariable UUID teamId,
                                                      @Parameter(name = "memberId", description = "member's' id", in = ParameterIn.PATH) @PathVariable UUID memberId) {
         ExitTeamResponse exitTeamResponse = teamApplicationService.exitTeam(new ExitTeamCommand(teamId, memberId));
-        return ResponseEntity.ok(exitTeamResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(exitTeamResponse));
     }
 
     /**
@@ -109,7 +107,6 @@ public class TeamController {
      *
      * @param teamId   팀 id
      * @param memberId 멤버 id
-     * @return
      */
     @Operation(summary = "팀 수정하기")
     @ApiResponses(value = {
@@ -118,18 +115,17 @@ public class TeamController {
                     "팀을 찾을 수 없습니다.")
     })
     @PutMapping(value = "/{teamId}")
-    public ResponseEntity<EditTeamResponse> editTeamInfo(@Parameter(name = "teamId", description = "team's' id", in = ParameterIn.PATH) @PathVariable UUID teamId,
+    public ResponseEntity<BaseResponse<EditTeamResponse>> editTeamInfo(@Parameter(name = "teamId", description = "team's' id", in = ParameterIn.PATH) @PathVariable UUID teamId,
                                                          @Parameter(name = "memberId", description = "member's' id", in = ParameterIn.QUERY) @RequestParam UUID memberId,
                                                          @RequestBody EditTeamRequest editTeamRequest) {
         EditTeamResponse editTeamResponse = teamApplicationService.editTeamInfo(new EditTeamInfoCommand(teamId, memberId, editTeamRequest));
-        return ResponseEntity.ok(editTeamResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(editTeamResponse));
     }
 
     /**
      * 멤버가 속한 팀 리스트 조회 api
      *
      * @param memberId 멤버 id
-     * @return
      */
     @Operation(summary = "멤버가 속한 팀 리스트 조회")
     @ApiResponses(value = {
@@ -138,8 +134,8 @@ public class TeamController {
                     "팀을 찾을 수 없습니다.")
     })
     @GetMapping(value = "")
-    public ResponseEntity<List<GetMemberTeam> > findMemberTeamList(@Parameter(name = "memberId", description = "member's' id", in = ParameterIn.QUERY) @RequestParam UUID memberId) {
+    public ResponseEntity<BaseResponse<List<GetMemberTeam>>> findMemberTeamList(@Parameter(name = "memberId", description = "member's' id", in = ParameterIn.QUERY) @RequestParam UUID memberId) {
         List<GetMemberTeam>  getMemberTeamList = teamApplicationService.findMemberTeamList(new GetMemberTeamCommand(memberId));
-        return ResponseEntity.ok(getMemberTeamList);
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(getMemberTeamList));
     }
 }
