bc4d8b2a21adeb06e75af3e672a845f74f023385
염석천 <90550303+seock1000@users.noreply.github.com>
2023-11-09 16:50:18 +0900

    [FIX] : notification-service Swagger 설정 수정 (#96)

--- notification-service/notification-container/src/main/java/com/garamgaebee/notification/service/domain/config/NotificationOpenApiConfig.java
+++ notification-service/notification-container/src/main/java/com/garamgaebee/notification/service/domain/config/NotificationOpenApiConfig.java
@@ -1,6 +1,8 @@
 package com.garamgaebee.notification.service.domain.config;
 
 import com.garamgaebee.common.config.OpenApiConfig;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class NotificationOpenApiConfig extends OpenApiConfig {
 }
